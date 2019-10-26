12b38e0c7f51bef78201bb4cd7de7bda1d872873
Azfar Mohamed <azfarmah@outlook.com>
2019-10-26 21:39:18 +0000

    Automatically backed up by Learn

--- spec/c++/g.cpp
+++ spec/c++/g.cpp
@@ -12,8 +12,12 @@ int main(){
   for(int i =0 ; i < n ; i++) cin >> d[i];
   map<int,int> g;
   for(int i : 0; i < d.length(); i++)
+  {
     g[d[i]]++
+  }
   for (int i : 0; i < d.length(); i++)
+  {
     if (g[v[i]) ==1)
       cout<<i<<endl;
+    }
 }
