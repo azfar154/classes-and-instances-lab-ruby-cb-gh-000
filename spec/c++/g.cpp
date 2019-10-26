4c559b9bb0c51cbbea7e8eaaa4f2f37c2ee82814
Azfar Mohamed <azfarmah@outlook.com>
2019-10-26 21:36:55 +0000

    Automatically backed up by Learn

--- spec/c++/g.cpp
+++ spec/c++/g.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 int main(){
   freopen("g.in","r",stdin);
   freopen("g.out","w",stdout);
   int n;
   cin >> n;
-  vector <int> g(n);
-  for(int i =0 ; i < n ; i++) cin >> g[i];
-
+  vector <int> d(n);
+  for(int i =0 ; i < n ; i++) cin >> d[i];
+  map<int,int> g;
+  for(i : d)
+    g[i]++
+  for (i : g)
+    if (g[i] ==1)
+      cout<<i<<endl;
 }
